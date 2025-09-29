e6886485d3416be5af8f6ec5b556f7d10556aad2
Tin210303 <tinnguyen210303@gmail.com>
2025-09-29 08:24:55 +0700

    Tin Edit Sidebar

--- social-project/src/layouts/MasterLayout.tsx
+++ social-project/src/layouts/MasterLayout.tsx
@@ -7,19 +7,14 @@ import RightSidebar from "../component_helper/RightSidebar"
 const MasterLayout = () => {
     return (
         <div className='main-layout'>
-            <div className="d-flex col-12">
-                <div className="col-2">
-                    <Sidebar />
-                </div>
-                <div className="bg-white h-100vh w-100 hide-scrollbar col-7" style={{ overflowY: 'auto' }}>
+            <div className="d-flex">
+                <Sidebar />
+                <div className="bg-white h-100vh w-100 hide-scrollbar" style={{ overflowY: 'auto' }}>
                     <Header />
                     <div className="d-flex">
                         <Outlet />
                         <RightSidebar />
                     </div>
-                </div>
-                <div className="col-3">
-                    <MessengerSidebar />
                 </div>
             </div>
         </div>
